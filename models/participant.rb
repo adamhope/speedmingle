e0bb36a5288dde55cfd4f3aef4cc8841a7583a0e
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-08-26 20:06:06 +1000

    Added validation to participant to check presence of

--- models/participant.rb
+++ models/participant.rb
@@ -3,7 +3,7 @@ require 'mongo_mapper'
 class Participant
   include MongoMapper::Document
   key :voted_for_by, Array
-  key :phone_number, String, :unique => true
+  key :phone_number, String, :unique => true, :required => true
   def score
     voted_for_by.length
   end
