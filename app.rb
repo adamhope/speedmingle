53f0aadd31ec36860b07bf2ff9507b099aaf2949
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-08-30 11:29:44 +1000

    Chanegs to database yaml

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
-require './config/init'
+require 'json'
+require 'slim'
+require 'sass/plugin/rack'
+require 'require_all'
+require_all 'config/database'
+require_all 'services'
+require_all 'models'
+
+Sass::Plugin.options[:style] = :compressed
+use Sass::Plugin::Rack
+
 require 'sinatra/partial'
 require 'pry-debugger'
 
