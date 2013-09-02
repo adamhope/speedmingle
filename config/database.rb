d8fe3b24f840c7877f047f72d89c4fd12760945b
Frederic Lemay <frederic.lemay@gmail.com>
2013-09-02 20:33:52 +1000

    cleaned up database.rb to use the settings instead!

--- config/database.rb
+++ config/database.rb
@@ -2,12 +2,5 @@ require 'mongo_mapper'
 require 'uri'
 require 'mongo_mapper'
 
-# Configure the environment
-if ENV['MONGOLAB_URI']
-  mongo_url = ENV['MONGOLAB_URI']
-  MongoMapper.connection = Mongo::Connection.from_uri mongo_url
-else
-  MongoMapper.connection = Mongo::Connection.new(settings.db_host, settings.db_port)
-  MongoMapper.database = settings.db_name
-  MongoMapper.connection.connect
-end
+MongoMapper.connection = Mongo::Connection.from_uri settings.db_uri
+MongoMapper.database = settings.db_name
