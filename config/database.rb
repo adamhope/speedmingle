33b7d1798d803a384b403d26a5287db00012b8a1
Frederic Lemay <frederic.lemay@gmail.com>
2013-09-02 19:07:26 +1000

    added a config file and configure test to use test database instead of dev one

--- config/database.rb
+++ config/database.rb
@@ -2,16 +2,12 @@ require 'mongo_mapper'
 require 'uri'
 require 'mongo_mapper'
 
-# # Configure the environment
+# Configure the environment
 if ENV['MONGOLAB_URI']
   mongo_url = ENV['MONGOLAB_URI']
   MongoMapper.connection = Mongo::Connection.from_uri mongo_url
-  MongoMapper.database = URI.parse(mongo_url).path.gsub(/^\//, '') #Extracts 'dbname' from the uri
 else
-  db_host = "localhost"
-  db_port = "27017"
-  db_name = test? ? "speedmingle-test" : "speedmingle-dev"
-  MongoMapper.connection = Mongo::Connection.new(db_host, db_port)
-  MongoMapper.database = db_name
+  MongoMapper.connection = Mongo::Connection.new(settings.db_host, settings.db_port)
+  MongoMapper.database = settings.db_name
   MongoMapper.connection.connect
 end
--- config/init.rb
+++ config/init.rb
@@ -1,7 +1,11 @@
 require 'json'
 require 'slim'
 require 'sass/plugin/rack'
+require "sinatra/config_file"
 require 'require_all'
+
+config_file 'config/config.yml'
+
 require_all 'config/database'
 require_all 'services'
 require_all 'models'
