830648030661d922cdb823760da11ab5922db7ad
Frederic Lemay <frederic.lemay@gmail.com>
2013-09-03 19:44:19 +1000

    yml does not recognise any erb..wonder why I put that in here...what a shame! Anyways, init.rb contains settings!

--- config/database.rb
+++ config/database.rb
@@ -2,10 +2,5 @@ require 'mongo_mapper'
 require 'uri'
 require 'mongo_mapper'
 
-if ENV['MONGOLAB_URI']
-  MongoMapper.connection = Mongo::Connection.from_uri ENV['MONGOLAB_URI']
-  MongoMapper.database = URI.parse(ENV['MONGOLAB_URI']).path.gsub(/^\//, '')
-else
-  MongoMapper.connection = Mongo::Connection.from_uri settings.db_uri
-  MongoMapper.database = URI.parse(settings.db_uri).path.gsub(/^\//, '')
-end
+MongoMapper.connection = Mongo::Connection.from_uri settings.db_uri
+MongoMapper.database = URI.parse(settings.db_uri).path.gsub(/^\//, '')
--- config/init.rb
+++ config/init.rb
@@ -4,7 +4,27 @@ require 'sass/plugin/rack'
 require "sinatra/config_file"
 require 'require_all'
 
-config_file 'config/config.yml'
+configure :development do
+  set :db_uri, 'mongodb://localhost/speedmingle-dev'
+  set :auth_username, ''
+  set :auth_password, ''
+end
+
+configure :test do
+  set :db_uri, 'mongodb://localhost/speedmingle-test'
+  set :auth_username, ''
+  set :auth_password, ''
+end
+
+configure :production do
+  set :db_uri, ENV['MONGOLAB_URI']
+  set :auth_username, ENV['SETTINGS_AUTH_USERNAME'] || 'needtobechanged'
+  set :auth_password, ENV['SETTINGS_AUTH_PASSWORD'] || 'needtobechanged'
+  set :burst_api_url, ENV['BURST_API_URL']
+  set :burst_api_key, ENV['BURST_API_KEY']
+  set :burst_api_secret, ENV['BURST_API_SECRET']
+  set :burst_caller_id, ENV['BURST_CALLER_ID']
+end
 
 require_all 'config/database'
 require_all 'services'
