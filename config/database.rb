2eaed7a7ad3868d9d5ee1b11f138313684a52e26
Frederic Lemay <frederic.lemay@gmail.com>
2013-09-03 18:45:51 +1000

    some debugging...

--- config/database.rb
+++ config/database.rb
@@ -2,5 +2,12 @@ require 'mongo_mapper'
 require 'uri'
 require 'mongo_mapper'
 
-MongoMapper.connection = Mongo::Connection.from_uri settings.db_uri
-MongoMapper.database = URI.parse(settings.db_uri).path.gsub(/^\//, '')
+if ENV['MONGOLAB_URI']
+  puts ENV['RACK_ENV'] 
+  puts settings.db_uri
+  MongoMapper.connection = Mongo::Connection.from_uri ENV['MONGOLAB_URI']
+  MongoMapper.database = URI.parse(ENV['MONGOLAB_URI']).path.gsub(/^\//, '')
+else
+  MongoMapper.connection = Mongo::Connection.from_uri settings.db_uri
+  MongoMapper.database = URI.parse(settings.db_uri).path.gsub(/^\//, '')
+end
