5c950071397a2e79c3095d2150694db9fe09471b
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-09-09 13:41:58 +1000

    Removed score because too much hassle to fix test

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ end
 # Returns all participants in database
 get '/participants' do
   content_type :json
-  Participant.sort(:score).to_json(:methods => [:score])
+  Participant.sort(:score).to_json
 end
 
 # /sms/dispatch/?mobile=1234567890&response=Fred&message_id=0
