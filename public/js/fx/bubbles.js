24d06da0745e783a46205256308f72eeb51104cf
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-11-16 17:06:01 +1100

    Added spawning off screen code

--- public/js/fx/bubbles.js
+++ public/js/fx/bubbles.js
@@ -13,17 +13,12 @@ function bubble(opts) {
       .size([w, h])
       .padding(1.5);
 
-
   var svg = d3.select("#bubbles").append("svg:svg")
     .attr("width", w)
     .attr("height", h)
     .attr("class", "bubble");
 
-  var generateColor = function() {
-    return ('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
-  };
-
-  var render = function(data) {  
+  var render = function(data) {
     var packedBubbleLayout = bubble.nodes({ children: data })
       .filter(function(d) { return !d.children; });
 
@@ -32,7 +27,10 @@ function bubble(opts) {
       
     nodes.enter().append("g")
       .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+      .attr("transform", function(d) { 
+        var spawnPosition = randomOffScreenPosition();
+        return "translate(" + spawnPosition.x * 2 + "," + spawnPosition.y * 2 + ")"; 
+      })
       .append("circle")
         .style("fill", function(d) { return generateColor(); })
         .attr("r", 0)
@@ -57,12 +55,24 @@ function bubble(opts) {
       .duration(1000)
       .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
 
-    
-    
     circles.transition()
       .duration(1000)
       .attr("r", function (d) { return d.r; })
   }
+
+  var generateColor = function() {
+    return ('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
+  };
+
+  var randomOffScreenPosition = function() {
+    return {
+      1: { x: _.random(0, w), y: 0 },
+      2: { x: _.random(0, w), y: h },
+      3: { x: 0, y: _.random(0, h) },
+      4: { x: w, y: _.random(0, h) }
+    }[_.sample([1, 2, 3, 4])]
+  }
+
 
   return {
     render: render
