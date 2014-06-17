eaee9800d4f453c733c39b36e66bf2903a241701
Ashok <ashokk@thoughtworks.com>
2014-06-17 20:10:39 +1000

    Revert "Revert "Ashok - Increased the delay""
    This reverts commit 5a3caf3c3d96cbf49eddb8ce57498e1e502968fa.

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -77,10 +77,10 @@ var ParticipantsController = app.controller("ParticipantsController", function($
 app.controller('DashboardSlideCtrl', ['$timeout',
     function(timeout) {
         var SLIDE_COUNT = 3;
-        var TIMEOUT = 5000;
+        var TIMEOUT = 25000;
         var self = this;
         var nth = 0;
-        var slides = ['instructions', 'bubble', 'connections'];
+        var slides = ['connections','instructions', 'bubble'];
 
         self.slide = slides[0];
 
